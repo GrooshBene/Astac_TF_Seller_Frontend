e17af519579a44dac687ba7cc26770fc23f562a3
박지수 <wltn000129@gmail.com>
2016-09-23 16:52:19 +0900

    App First Setting Done

---
+++ TodaysFeelingSeller/app/src/main/java/com/example/grooshbene/todaysfeelingseller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.grooshbene.todaysfeelingseller;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by GrooshBene on 2016. 9. 23..
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
