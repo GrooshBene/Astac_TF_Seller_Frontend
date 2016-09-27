0e64c9a7dc6864efe2a942b74c017ea3df9a14b5
박지수 <wltn000129@gmail.com>
2016-09-28 00:31:02 +0900

    TabBar and Fragments Changed Info TabHost and Activity

--- TodaysFeelingSeller/app/src/main/java/com/example/grooshbene/todaysfeelingseller/MainActivity.java
+++ TodaysFeelingSeller/app/src/main/java/com/example/grooshbene/todaysfeelingseller/MainActivity.java
@@ -1,28 +1,48 @@
 package com.example.grooshbene.todaysfeelingseller;
 
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TabHost;
 
 /**
  * Created by GrooshBene on 2016. 9. 23..
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
     Toolbar toolbar;
     TabLayout tabLayout;
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        tabLayout = (TabLayout)findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("a"));
-        tabLayout.addTab(tabLayout.newTab().setText("b"));
-        tabLayout.addTab(tabLayout.newTab().setText("c"));
-        tabLayout.addTab(tabLayout.newTab().setText("d"));
+        TabHost tabhost = getTabHost();
+        TabHost.TabSpec spec;
+        Intent intent;
+        intent = new Intent().setClass(this, MonitorActivity.class);
+        spec = tabhost.newTabSpec("1");
+        spec.setIndicator("1");
+        spec.setContent(intent);
+        tabhost.addTab(spec);
+        intent = new Intent().setClass(this,InfoActivity.class);
+        spec = tabhost.newTabSpec("2");
+        spec.setIndicator("2");
+        spec.setContent(intent);
+        tabhost.addTab(spec);
+        intent = new Intent().setClass(this,StatisticsActivity.class);
+        spec = tabhost.newTabSpec("3");
+        spec.setIndicator("3");
+        spec.setContent(intent);
+        tabhost.addTab(spec);
+        intent = new Intent().setClass(this, PlaceInfoActivity.class);
+        spec = tabhost.newTabSpec("4");
+        spec.setIndicator("4");
+        spec.setContent(intent);
+        tabhost.addTab(spec);
+        tabhost.setCurrentTab(0);
     }
 }
---
+++ TodaysFeelingSeller/app/src/main/java/com/example/grooshbene/todaysfeelingseller/PlaceInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.grooshbene.todaysfeelingseller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by GrooshBene on 2016. 9. 28..
+ */
+public class PlaceInfoActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_placeinfo);
+    }
+}
